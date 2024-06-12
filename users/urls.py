01cb2200ae84c19483ea884153881fb412ffbb98
Maria Lipa <machinatororis@gmail.com>
2024-06-12 22:04:22 +0300

    added: registration page

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,12 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = "users"
 urlpatterns = [
     # ввімкнути URL авторизації за замовченням
     path("", include("django.contrib.auth.urls")),
+    # Сторінка реєстрації
+    path("register/", views.register, name="register"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register(request):
+    """Реєстрація нового користувача"""
+    # Перевіряємо, чи відповідає ф-ція на запит POST
+    if request.method != "POST":
+        # Показуємо порожню форму реєстрації
+        form = UserCreationForm()
+    else:
+        # Обробка заповненної форми
+        # Створюємо екземпляр класу UserCreationForm на основі відправлених даних
+        form = UserCreationForm(data=request.POST)
+
+        # Перевіряємо, чи вірні дані
+        if form.is_valid():
+            new_user = form.save()  # зберігаємо ім'я користувача і хеш пароля в БД
+            # Виконання входу і перенаправлення на домашню сторінку
+            login(request, new_user)
+            return redirect("learn_trackers:index")
+
+    # Показуємо порожню або недійсну форму
+    context = {"form": form}
+    return render(request, "registration/register.html", context)
